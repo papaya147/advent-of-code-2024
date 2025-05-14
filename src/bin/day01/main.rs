1b88063cd0a29ecbb243e7c27c2f5384cda59ad3
Abhinav Srivatsa <asrivatsa6@gmail.com>
2025-05-14 19:22:45 +0530

    simplify errors in day 1

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,24 +1,8 @@
-use std::error::Error as StdError;
-use std::fmt::Display;
+use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Debug)]
-enum Error {
-    InputLengthMismatchErr,
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::InputLengthMismatchErr => write!(f, "input length mismatch"),
-        }
-    }
-}
-
-impl StdError for Error {}
-
-fn parse_input(file_path: &str) -> Result<[Vec<u32>; 2], Box<dyn StdError>> {
+fn parse_input(file_path: &str) -> Result<[Vec<u32>; 2], Box<dyn Error>> {
     let mut locs = [Vec::new(), Vec::new()];
 
     let file = File::open(file_path)?;
@@ -26,7 +10,7 @@ fn parse_input(file_path: &str) -> Result<[Vec<u32>; 2], Box<dyn StdError>> {
         let line = line?;
         let mut split = line.split_whitespace();
         let (Some(loc_1), Some(loc_2)) = (split.next(), split.next()) else {
-            return Err(Box::new(Error::InputLengthMismatchErr));
+            panic!("invalid line size")
         };
         locs[0].push(loc_1.parse()?);
         locs[1].push(loc_2.parse()?);
